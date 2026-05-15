cafd66e15d97c8e12c8e54ff2a44e55705f5be5f
PoojaNL0504 <poojanl.work5@gmail.com>
2026-05-15 12:22:24 +0530

    updated main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,6 +16,7 @@ app.add_middleware(
    allow_origins=[
         "https://resume-ai-projects.vercel.app"
     ],
+    access_control_allow_origin="https://resume-ai-projects.vercel.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
